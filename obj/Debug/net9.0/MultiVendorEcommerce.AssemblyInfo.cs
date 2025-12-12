1f144df692a2edf20ce183946b47c76c0425f538
factorgh <burchellsbale@gmail.com>
2025-12-12 21:22:00 +0000

    update on backend api

--- obj/Debug/net9.0/MultiVendorEcommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/MultiVendorEcommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MultiVendorEcommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48312403482d7f78616a558abe35efc92b1697ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b23fe4fb86df7458203c6725e5ecdaa05548013")]
 [assembly: System.Reflection.AssemblyProductAttribute("MultiVendorEcommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MultiVendorEcommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
